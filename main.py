acdcd5eb16d9c9c6b3a299ee633f80890a043e46
eaydogdu <eaydogdu@biggloyalty.com>
2019-03-19 15:37:12 +0100

    update

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import tweepy
 from textblob import TextBlob
 import csv
 
-consumer_key = 'YOUR_CONSUMER_TOKEN'
-consumer_secret = 'YOUR_CONSUMER_SECRET'
+consumer_key = 'ur43PXS7FgMvWtX7yQ4BXY7oj'
+consumer_secret = 'j4nJlKvejCAIZvIAee2Y77SZgj1C5jcgXyEpP4qmdyfCq39KCj'
 
-access_token ='YOUR_ACCESS_TOKEN'
-access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
+access_token ='257074402-NjWt90wfV0Ft7zzsoaFArSH4H5iGfV56OqoS0k1V'
+access_token_secret = '4gM3vBobYGy6vjSBeE3MeUXw54Gk0fOyVB8HDmphzSvb5'
 
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
